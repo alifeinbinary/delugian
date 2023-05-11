f3af9373eb8b376c140a9eea83ec4d1cd2a0b697
Andrew Halliwell <studio@strangeways.ca>
2023-05-11 03:41:00 -0700

    Adding MIDI communication and device detection

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,114 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use midir::{Ignore, MidiInput, MidiInputConnection, MidiOutput, MidiOutputConnection};
+use serde::Serialize;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+use tauri::{Manager, Window, Wry};
+
+#[derive(Default)]
+pub struct MidiState {
+    pub input: Mutex<Option<MidiInputConnection<()>>>,
+    pub output: Mutex<Option<MidiOutputConnection>>,
+}
+
+#[derive(Clone, Serialize)]
+struct MidiMessage {
+    message: Vec<u8>,
+}
+
+#[tauri::command]
+fn list_midi_connections() -> HashMap<usize, String> {
+    let midi_in = MidiInput::new("delugian-input");
+    match midi_in {
+        Ok(midi_in) => {
+            let mut midi_connections = HashMap::new();
+            for (i, p) in midi_in.ports().iter().enumerate() {
+                let port_name = midi_in.port_name(p);
+                match port_name {
+                    Ok(port_name) => {
+                        midi_connections.insert(i, port_name);
+                    }
+                    Err(e) => {
+                        println!("Error getting port name: {}", e);
+                    }
+                }
+            }
+            midi_connections
+        }
+        Err(_) => HashMap::new(),
+    }
+}
+
+#[tauri::command]
+fn open_midi_connection(
+    midi_state: tauri::State<'_, MidiState>,
+    window: Window<Wry>,
+    input_idx: usize,
+) {
+    let handle = Arc::new(window).clone();
+    let midi_in = MidiInput::new("delugian-input");
+    let midi_out = MidiOutput::new("delugian-output");
+    match midi_in {
+        Ok(mut midi_in) => {
+            midi_in.ignore(Ignore::None);
+            let midi_in_ports = midi_in.ports();
+            let port = midi_in_ports.get(input_idx);
+            match port {
+                Some(port) => {
+                    let midi_in_conn = midi_in.connect(
+                        port,
+                        "midir",
+                        move |_, message, _| {
+                            handle
+                                .emit_all(
+                                    "midi_message",
+                                    MidiMessage {
+                                        message: message.to_vec(),
+                                    },
+                                )
+                                .map_err(|e| {
+                                    println!("Error sending midi message: {}", e);
+                                })
+                                .ok();
+                        },
+                        (),
+                    );
+                    match midi_in_conn {
+                        Ok(midi_in_conn) => {
+                            midi_state.input.lock().unwrap().replace(midi_in_conn);
+                        }
+                        Err(e) => {
+                            println!("Error: {}", e);
+                        }
+                    }
+                }
+                None => {
+                    println!("No port found at index {}", input_idx);
+                }
+            }
+        }
+        Err(e) => println!("Error: {}", e),
+    }
+    match midi_out {
+        Ok(midi_out) => {
+            let midi_out_ports = midi_out.ports();
+            let port = midi_out_ports.get(input_idx);
+            match port {
+                Some(port) => {
+                    let midi_out_conn = midi_out.connect(port, "midir").unwrap();
+                    midi_state.output.lock().unwrap().replace(midi_out_conn);
+                }
+                None => {
+                    println!("No port found at index {}", input_idx);
+                }
+            }
+        }
+        Err(e) => println!("Error: {}", e),
+    }
+}
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -9,7 +117,15 @@ fn greet(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .manage(MidiState {
+            ..Default::default()
+        })
+        .invoke_handler(tauri::generate_handler![
+            greet,
+            open_midi_connection,
+            list_midi_connections
+        ])
+        .plugin(tauri_plugin_store::Builder::default().build())
         .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .expect("error while running delugian application");
 }
